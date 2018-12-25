0f24eda61c3e7f589530e584e01962a6edd51b5d
rsun624 <raymondsun@berkeley.edu>
2018-12-24 22:55:39 -0500

    Finished author_age.rb

--- week1/ch02/author_age.rb
+++ week1/ch02/author_age.rb
@@ -5,3 +5,6 @@
 
 
 ### Your Code Here ###
+puts "The author is 1160000000 seconds old."
+puts "There are 60 seconds in a minute, 60 minutes in an hour, 24 hours in a day, and 365.25 days in a year."
+puts "Thus, the author is " + (((((1160000000.0/60.0)/60.0)/24.0)/365.25).to_int).to_s + " years old"
