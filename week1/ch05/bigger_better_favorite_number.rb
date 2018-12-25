df6fc4fca45786691961f9e1ac5955b494a1d401
rsun624 <raymondsun@berkeley.edu>
2018-12-25 00:26:02 -0500

    Finished bigger_better_favorite_number

--- week1/ch05/bigger_better_favorite_number.rb
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -4,3 +4,11 @@
 
 
 ### Your Code Here ###
+puts "Hi there! I'm a mathematician collecting data about people's favorite numbers."
+puts "What's your favorite number?"
+fnumber=gets.chomp
+puts "Hm. Are you sure about choosing " + fnumber + " as your favorite number?"
+puts "That number is pretty lame. I suggest " + ((fnumber.to_i)+1).to_s + "."
+puts "As a mathematician, I can certifiably say that this is a bigger and better number."
+puts "Please, if you have any dignity, do consider choosing this superior number as a favorite."
+puts "Have a nice day, imbecile."
