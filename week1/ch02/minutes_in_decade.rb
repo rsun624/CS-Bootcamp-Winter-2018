a77e5d99e940e99b5dce8e0a0c0e49b4fa2dc162
rsun624 <raymondsun@berkeley.edu>
2018-12-24 23:08:34 -0500

    Finished minutes_in_decade

--- week1/ch02/minutes_in_decade.rb
+++ week1/ch02/minutes_in_decade.rb
@@ -3,3 +3,9 @@
 
 
 ### Your Code Here ###
+puts "A leap year is every four years. Thus, in a decade, there can be either 2 or 3 leap years."
+puts "The average number of leap years in a decade is then " + (10.0/4.0).to_s + "."
+puts "The average number of non-leap years in a decade is " + (10.0-(10.0/4.0)).to_s + "."
+puts "Thus, the average number of days in a decade is " + (((10.0/4.0)*366.0)+((10.0-(10.0/4.0))*365.0)).to_s + "."
+puts "There are 24 hours in a day, and 60 minutes in an hour."
+puts "Thus, there are an average " + (((((10.0/4.0)*366.0)+((10.0-(10.0/4.0))*365.0))*24.0*60.0).to_int).to_s + " minutes in a decade."
