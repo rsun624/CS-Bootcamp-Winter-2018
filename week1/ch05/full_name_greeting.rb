c616e305e86cdf4b323361150031a7bb5e546853
rsun624 <raymondsun@berkeley.edu>
2018-12-25 00:15:19 -0500

    Finished full_name_greeting

--- week1/ch05/full_name_greeting.rb
+++ week1/ch05/full_name_greeting.rb
@@ -4,3 +4,13 @@
 
 
 ### Your Code Here ###
+puts "Welcome to the DMV. Thank you for waiting four hours to get your driver's license renewed."
+puts "I'm just going to ask for some information if you don't mind."
+puts "What's your first name?"
+fname=gets.chomp
+puts "Thank you. What's your middle name?"
+mname=gets.chomp
+puts "Cheers. Please give me your last name."
+lname=gets.chomp
+puts "Awesome. Nice to meet you, " + fname + " " + mname + " " + lname + "."
+puts "I'll get you your license after I finish my lunch break. See you in two hours."
