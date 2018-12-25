051b89853c9789ed88bbaa639245b2bc2d1b124f
rsun624 <raymondsun@berkeley.edu>
2018-12-24 23:14:46 -0500

    Finished hours_in_year

--- week1/ch02/hours_in_year.rb
+++ week1/ch02/hours_in_year.rb
@@ -1,1 +1,1 @@
-puts "A normal year has 365 days and a leap year has 366 days. Each day has 24 hours. If every four years there is one leap year, then that means that the average year has " + ((((365.0*3.0)+366.0)/4.0)*24.0).to_s + " hours."
+puts "A normal year has 365 days and a leap year has 366 days. Each day has 24 hours. If every four years there is one leap year, then that means that the average year has " + (((((365.0*3.0)+366.0)/4.0)*24.0).to_int).to_s + " hours."
