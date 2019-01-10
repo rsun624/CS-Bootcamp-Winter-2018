d93ec5a7d3be750b5bb1cef25109172a016635ea
rsun624 <raymondsun@berkeley.edu>
2019-01-10 14:14:25 -0500

    Finished Lab 3 and the questionnaire

---
+++ week3/ch09/ask.rb
@@ -0,0 +1,43 @@
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    if (reply == "yes"||reply == "no")
+      if reply == "yes"
+        return true
+      else
+        return false
+      end
+      break
+    else
+      puts "Please answer yes or no"
+    end
+  end
+
+end
+puts 'Hello, and thank you for taking the time to
+help me with this experiment. My experiment
+has to do with the way people feel about
+Mexican food. Just think about Mexican food
+and try to answer every question honestly,
+with either a "yes" or a "no". My experiment
+has nothing to do with bed-wetting.'
+ask 'Do you like eating tacos?' # Ignore this return value
+ask 'Do you like eating burritos?' # And this one
+wets_bed = ask 'Do you wet the bed?' # Save this return value
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas?'
+puts 'Just a few more questions...'
+ask 'Do you like drinking horchata?'
+ask 'Do you like eating flautas?'
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for taking the time to help with
+this experiment. In fact, this experiment
+has nothing to do with Mexican food. It is
+an experiment about bed-wetting. The Mexican
+food was just there to catch you off guard
+in the hopes that you would answer more
+honestly. Thanks again.'
+puts
+puts wets_bed
